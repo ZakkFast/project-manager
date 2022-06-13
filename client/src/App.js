0b66e890f57e9d8b0122b1408e2ab93853afae93
Zakk Fast <zakkfastpro@gmail.com>
2022-06-12 22:56:21 -0700

    created react app and cleaned up files

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+// Components
+import Header from './components/layout/Header';
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="container">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
