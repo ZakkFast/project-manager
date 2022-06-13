df8fa0aa1022b87156277315ae252c5dca25d1e6
Zakk Fast <zakkfastpro@gmail.com>
2022-06-12 23:25:56 -0700

    set up render for clients list

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
+import { ApolloProvider, ApolloClient, InMemoryCache } from '@apollo/client';
+
 // Components
 import Header from './components/layout/Header';
+import Clients from './components/Clients';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:5000/graphql',
+  cache: new InMemoryCache(),
+});
 
 function App() {
   return (
     <>
-      <Header />
-      <div className="container">
-        <h1>Hello World</h1>
-      </div>
+      <ApolloProvider client={client}>
+        <Header />
+        <div className="container">
+          <Clients />
+        </div>
+      </ApolloProvider>
     </>
   );
 }
