89e369e3274585e63243f45e467b1c85b2ee32bd
Zakk Fast <zakkfastpro@gmail.com>
2022-06-12 22:08:13 -0700

    set up graphql queries and mongo connection

---
+++ server/schema/schema.js
@@ -0,0 +1,75 @@
+// Mongoose Models
+const Project = require('../models/Project');
+const Client = require('../models/Client');
+
+const {
+  GraphQLObjectType,
+  GraphQLID,
+  GraphQLString,
+  GraphQLSchema,
+  GraphQLList,
+} = require('graphql');
+
+// Client Tpye
+const ClientType = new GraphQLObjectType({
+  name: 'Client',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    email: { type: GraphQLString },
+    phone: { type: GraphQLString },
+  }),
+});
+
+// Project Type
+const ProjectType = new GraphQLObjectType({
+  name: 'Project',
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+    status: { type: GraphQLString },
+    client: {
+      type: ClientType,
+      resolve(parent, args) {
+        return clients.find((client) => client.id === parent.clientId);
+      },
+    },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: 'RootQueryType',
+  fields: {
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parents, args) {
+        Project.find();
+      },
+    },
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return Project.findById(args.id);
+      },
+    },
+    projects: {
+      type: new GraphQLList(ProjectType),
+      resolve(parents, args) {
+        return Project.findById(args.id);
+      },
+    },
+  },
+  project: {
+    type: ProjectType,
+    args: { id: { type: GraphQLID } },
+    resolve(parent, args) {
+      return Project.find();
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
